4d8918edb1674f30f0031eafb5441a24341f24df
Fafy <fafy@gflclan.com>
2020-08-04 12:26:31 -0300

    Defined config

--- linkfilter.py
+++ linkfilter.py
@@ -1,8 +1,22 @@
 from redbot.core import commands
+from redbot.core import Config
 import logging
 
 log = logging.getLogger("red.linkfilter.linkfilter")
 class LinkFilter(commands.Cog):
+	def __init__(self):
+		self.config = Config.get_conf(self, identifier=280102)
+		# Blacklist is shared between guilds
+		default_global = {
+			"blacklist": []
+		}
+		# The logs channel is defined per guild
+		default_guild = {
+			"logchannel": False
+		}
+		# Register default values
+		self.config.register_global(**default_global)
+		self.config.register_guild(**default_guild)
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
@@ -10,4 +24,6 @@ class LinkFilter(commands.Cog):
 		if message.author.bot or not message.guild:
 			return
 
-		
+	@commands.command()
+	async def test(self, ctx):
+		await ctx.send(await self.config.foo())
