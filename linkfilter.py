a2c720ceff66b3d1f8299a7e798e1271e40dba47
Fafy <fafy@gflclan.com>
2020-08-04 15:37:16 -0300

    Added list command and cache of the blacklist

--- linkfilter.py
+++ linkfilter.py
@@ -1,6 +1,7 @@
 from redbot.core import commands
 from redbot.core import Config
 from redbot.core import utils
+import discord
 
 class LinkFilter(commands.Cog):
 	def __init__(self):
@@ -13,19 +14,23 @@ class LinkFilter(commands.Cog):
 		self.config.register_guild(
 			logchannel = False
 		)
-	
-	def on_ready(self):
-		# Cache blacklist
-		self.blacklist = await self.config.blacklist()
+		# Dummy blacklist
+		self.blacklist = False
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
+		# Cache blacklist
+		if not self.blacklist:
+			self.blacklist = await self.config.blacklist()
 		# Stop recursion or private messages stuff
 		if message.author.bot or not message.guild:
 			return
 
 	@commands.command()
 	async def linkfilter(self, ctx, action, *args):
+		# Cache blacklist
+		if not self.blacklist:
+			self.blacklist = await self.config.blacklist()
 		# Defines log channel
 		if action == "log":
 			if len(ctx.message.channel_mentions) > 0:
@@ -42,6 +47,14 @@ class LinkFilter(commands.Cog):
 					blacklist.append(arg)
 				# Add to blacklist cache
 				self.blacklist.append(arg)	
-		
+		# Gets all blacklisted links
+		elif action == "list":
+			desc = ""
+
+			for i in range(0, len(self.blacklist)):
+				desc = desc + f"**{i + 1}:** {self.blacklist[i]}\n"
+
+			embed = discord.Embed(title="Blacklisted domains", description=desc, colour=ctx.author.colour)
+			await ctx.send(content=None, embed=embed)
 
 		#await ctx.send(await self.config.guild(ctx.guild).logchannel())
