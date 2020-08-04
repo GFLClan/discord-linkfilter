1778d4da1cd0394e757548ce839d03265b40af84
Fafy <fafy@gflclan.com>
2020-08-04 12:08:14 -0300

    Ignoring *.pyc

--- linkfilter.py
+++ linkfilter.py
@@ -1,11 +1,17 @@
 from redbot.core import commands
+import logging
 
+log = logging.getLogger("red.linkfilter.linkfilter")
 class LinkFilter(commands.Cog):
 	"""
-		Prevents malitious links from being posted
+		Prevents malicious links from being posted
 		This works in a blacklist fashion, where you prohibit domains
 	"""
 
-	@commands.command()
-	async def placeholder(self, ctx):
-		await ctx.send("HELL")
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		# Stop recursion or private messages stuff
+		if message.author.bot or not message.guild:
+			return
+
+		
